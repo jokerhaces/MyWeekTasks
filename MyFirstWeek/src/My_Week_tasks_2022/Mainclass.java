33582f8958ef6d9c4b45dca3df09ade47a32eed2
Jouko Romppainen <jokerhaces@gmail.com>
2022-06-07 11:58:59 +0300

    First initial commit

---
+++ MyFirstWeek/src/My_Week_tasks_2022/Mainclass.java
@@ -0,0 +1,9 @@
+package My_Week_tasks_2022;
+
+public class Mainclass {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Hello World!");
+    }
+}
