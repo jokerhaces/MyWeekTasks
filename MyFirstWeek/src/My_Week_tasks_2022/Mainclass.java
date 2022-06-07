577180554f6856781477a575d93df7ca44b65927
Jouko Romppainen <106674714+jokerhaces@users.noreply.github.com>
2022-06-07 15:15:25 +0300

    Delete MyFirstWeek/src/My_Week_tasks_2022 directory

--- MyFirstWeek/src/My_Week_tasks_2022/Mainclass.java
+++
@@ -1,9 +0,0 @@
-package My_Week_tasks_2022;
-
-public class Mainclass {
-
-    public static void main(String[] args) {
-	// write your code here
-        System.out.println("Hello World!");
-    }
-}
