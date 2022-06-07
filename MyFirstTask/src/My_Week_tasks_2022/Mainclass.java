4313471697e5ff84353ba6a597c86e8568ac1ad1
Jouko Romppainen <jokerhaces@gmail.com>
2022-06-07 15:46:58 +0300

    Initial commitment. Print Hello World! and adding comment lines for code

--- MyFirstTask/src/My_Week_tasks_2022/Mainclass.java
+++ MyFirstTask/src/My_Week_tasks_2022/Mainclass.java
@@ -1,8 +1,12 @@
 package My_Week_tasks_2022;
+// Mainclass.java Jouko Romppainen
+// Print Hello World!
+// Intellij Idea Community Edition 2021.1.1
+// MIT License
 
 public class Mainclass {
+
     public static void main(String[] args) {
-        //
         System.out.println("Hello World!");
     }
 }
